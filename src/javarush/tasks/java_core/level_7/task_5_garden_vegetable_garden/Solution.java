f6820806058783fb3e33307f7305d2c157f28043
user <user@userPC>
2022-04-29 15:23:27 +0300

    else

---
+++ src/javarush/tasks/java_core/level_7/task_5_garden_vegetable_garden/Solution.java
@@ -0,0 +1,19 @@
+package javarush.tasks.java_core.level_7.task_5_garden_vegetable_garden;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+	public Solution() {
+
+	}
+	
+	public class Garden{
+		
+		public final List<String> fruits = new ArrayList<>();
+		public final List<String> vegatable = new ArrayList<>();
+		
+	}
+
+}
